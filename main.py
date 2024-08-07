60d8a3f3d4a1f8789ab67290d06bd080d8a96794
ElisQuintao <elisangela.emq@gmail.com>
2024-08-07 08:38:12 -0300

    contadorLacoRepeticao

---
+++ main.py
@@ -0,0 +1,10 @@
+# laço de repetição - contador
+# contagem decrescente
+# entrada de dados
+numero = int(input('informe um numero inteiro: '))
+
+# saida de dados
+# -= é a mesma coisa de colocar a variavel - 1 ex. numero = numero-1
+while numero >= 1:
+    print(numero)
+    numero -= 1 
